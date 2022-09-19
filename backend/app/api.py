f60d2d0b564df92476f5e9a49c72c2ddc7077a4c
Reuben Cleetus <rcleetus@gmail.com>
2022-09-19 17:31:33 -0400

    uploadImage working

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+import random, string, os
 
+def get_storage_cn():
+    kv_name = "kvhack4skin"
+    kv_uri = f"https://{kv_name}.vault.azure.net"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=kv_uri, credential=credential)
+    secret = client.get_secret("storage-cn")
+    return secret.value
 
 app = FastAPI()
 
@@ -19,6 +30,15 @@ app.add_middleware(
 )
 
 
+@app.get('/eat', tags=["ingest"])
+def consume() -> dict:
+    return {"data":"Yes I can consume this too!"}
+
+def configure():
+    None
+
+configure()
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Hello World, I'm an API!"}
@@ -27,3 +47,18 @@ async def read_root() -> dict:
 @app.get("/upload", tags=["ingest"])
 async def upload_picture() -> dict:
     return {"data": "Whoo hoo!"}
+
+
+@app.post("/uploadImage/", tags=["ingest"])
+async def upload_image(file: UploadFile):
+# Upload an image
+    filename = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    file_split_tup = os.path.splitext(file.filename)
+    filename += file_split_tup[1]
+    storage_cn = get_storage_cn()   #Get storage account connection string
+    contents = await file.read()
+    blob = BlobClient.from_connection_string(storage_cn, container_name="uploads", blob_name=filename)
+    blob.upload_blob(contents)
+    print(f"Random Filename: {filename}")
+    print(f"Actual Filename: {file.filename}")
+    return {"filename": file.filename}
